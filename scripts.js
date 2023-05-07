cf2cddeab04d6e0c8c0aae6fb92c3e969e0dc3e2
Yutaku Kita <127103166+yutakukita@users.noreply.github.com>
2023-05-07 22:40:00 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,35 +1,14 @@
 // JavaScript Document
 
-//--- Adjust height of sections to fit the contents ---//
-// $(document).ready(function() {
-// 	// Loop through each section
-// 	$('#live').each(function() {
-// 	  // Get the height of the content inside the section
-// 	  var contentHeight = $(this).find('.container').outerHeight();
-// 	  // Set the section height to be the maximum of the window height and the content height
-// 	  $(this).css('height', Math.max($(window).height(), contentHeight) + 'px');
-// 	});
+//--- set background-attachment: touch for iPhones and iPads ---//
+if (/iPad|iPhone|iPod/.test(navigator.userAgent)) {
+	// User is browsing with an iPhone or iPad
+	var sections = document.querySelectorAll('.cd-fixed-bg');
+	sections.forEach(function(section) {
+	  section.style.backgroundAttachment = 'touch';
+	});
+  }
 
-// 	  // Adjust the section height when the window is resized
-// 	//   $(window).resize(function() {
-// 	// 	$('#live').each(function() {
-// 	// 	  var contentHeight = $(this).find('.container').outerHeight();
-// 	// 	  $(this).css('height', Math.max($(window).height(), contentHeight) + 'px');
-// 	// 	});
-// 	//   });
-	
-//   });
-  
-// #live
-// var live_section = document.getElementByld('live');
-// var live_container = document.getElementByld('live .container');
-// live_section.style.height = live_container.offsetHeight + 'px';
-// live_container.style.zindex = 1000;
-
-// #bio
-// var bio_section = document.getElementByld('bio');
-// var bio_container = document.getElementByld('bio .container');
-// bio_section.style.height = live_container.offsetHeight + 'px';
 
 //--- sns-link-show ---//
 jQuery(function($){
